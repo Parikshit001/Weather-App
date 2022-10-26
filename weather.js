1eef9e8be3c7a550e6ef6d8c07d4135d4cb05d71
Parikshit001 <parikshithiwase@gmail.com>
2022-10-27 00:25:10 +0530

    Praded the JSON, fetched the temprature and sky,made changes in the metric system

--- weather.js
+++ weather.js
@@ -1,8 +1,9 @@
+const { log } = require('console');
 const express = require('express');
 const app = express();
 const https = require('https');
 const apikey = "3f7a5a2567b483cd1fc2940be0db25ae" ;
-const url = "https://api.openweathermap.org/data/2.5/weather?q=nagpur&appid=" + apikey
+const url = "https://api.openweathermap.org/data/2.5/weather?q=nagpur&appid=" + apikey +"&units=metric"
 
 app.listen(3000, function(){
 
@@ -10,7 +11,17 @@ app.listen(3000, function(){
 });
 
 https.get(url,function(res){
-    console.log(res)
+    res.on("data", function(data){
+        var weatherinfo = JSON.parse(data);
+        var sky = weatherinfo.weather[0].main;
+        var temp = weatherinfo.main.temp
+        console.log(sky);
+        console.log(temp);
+        // weatherinfo.main.temp
+        // weatherinfo.weather.main
+    });
+
+    // console.log(res)
 })
 
 app.get('/',function(request, response){
